38262c277bd188005194f59a63333e380836df9e
drpriver <david@davidpriver.com>
2021-05-11 02:08:08 -0400

    Small wasm cleanups

--- Platform/Wasm/dndc_wasm.c
+++ Platform/Wasm/dndc_wasm.c
@@ -1,8 +1,26 @@
 #include "dndc.c"
 #include "dndc_flags.h"
 
+static
 void dndc_error_func(void* error_user_data, int type, const char* filename, int filename_len, int line, int col, const char* message, int message_len){
-    log_string(message, message_len);
+    if(message_len+filename_len < 4096-64){
+        MStringBuilder msb = {};
+        char buff[4096];
+        msb.data = buff;
+        msb.capacity = sizeof(buff);
+        msb_write_str(&msb, filename, filename_len);
+        msb_write_char(&msb, ':');
+        msb_write_int(&msb, line);
+        msb_write_char(&msb, ':');
+        msb_write_int(&msb, col);
+        msb_write_char(&msb, ':');
+        msb_write_str(&msb, message, message_len);
+        msb_nul_terminate(&msb);
+        log_string(buff, msb.cursor);
+        }
+    else{
+        log_string(message, message_len);
+        }
     }
 extern
 PString*
@@ -33,9 +51,14 @@ make_html(PString* source){
 printf_func(5, 6)
 extern
 void logfunc(int log_level, const char*_Nonnull file, const char*_Nonnull func, int line, const char*_Nonnull fmt, ...){
-    logi32(log_level);
-    log_string(file, strlen(file));
-    log_string(func, strlen(func));
-    logi32(line);
-    log_string(fmt, strlen(fmt));
+    MStringBuilder msb = {};
+    char buff[4096];
+    msb.data = buff;
+    msb.capacity = sizeof(buff);
+    msb_write_str(&msb, file, strlen(afile));
+    msb_write_str(&msb, func, strlen(func));
+    msb_write_int(&msb, line);
+    msb_write_str(&msb, fmt, strlen(fmt));
+    msb_nul_terminate(&msb);
+    log_string(buff, msb.cursor);
     }
--- Platform/Wasm/wasm.mak
+++ Platform/Wasm/wasm.mak
@@ -1,6 +1,3 @@
-ifeq ($(shell uname),Darwin)
-WASMCC=/usr/local/Cellar/llvm/9.0.1/bin/clang
-else
+# I think only clang supports wasm?
 WASMCC=clang
-endif
 WASMCFLAGS=--target=wasm32 --no-standard-libraries -Wl,--export-all -Wl,--no-entry -Wl,--allow-undefined -O3 -ffreestanding -nostdinc -isystem Platform/Wasm -DWASM
