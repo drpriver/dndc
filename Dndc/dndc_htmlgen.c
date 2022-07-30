69314fb8f1755deb27fc980a8e7baf2297553f27
drpriver <david@davidpriver.com>
2022-07-30 14:58:03 -0700

    allow <tt> tags

--- Dndc/dndc_htmlgen.c
+++ Dndc/dndc_htmlgen.c
@@ -500,7 +500,7 @@ write_link_escaped_str_slow(DndcContext* ctx, MStringBuilder* sb, const char* te
                 msb_write_literal(sb, "&amp;");
             }break;
             case '<':{
-                // we allow inline <b>, <s>, <i>, </b>, </s>, </i>, <br>, <code>, </code>, <hr>
+                // we allow inline <b>, <s>, <i>, </b>, </s>, </i>, <br>, <code>, </code>, <hr>, <tt>, </tt>
                 // This is a big mess and should be done in an easier to do way.
                 if(length - i >= 2){
                     char peek1 = text[i+1];
@@ -527,6 +527,22 @@ write_link_escaped_str_slow(DndcContext* ctx, MStringBuilder* sb, const char* te
                             if(memcmp(text+i, "</code>", sizeof("</code>")-1) == 0){
                                 msb_write_literal(sb, "</code>");
                                 i += sizeof("</code>")-2;
+                                continue;
+                            }
+                        }
+                        if(length - i >= sizeof("</tt>")-1){
+                            if(memcmp(text+i, "</tt>", sizeof("</tt>")-1) == 0){
+                                msb_write_literal(sb, "</tt>");
+                                i += sizeof("</tt>")-2;
+                                continue;
+                            }
+                        }
+                    }
+                    if(peek1 == 't'){
+                        if(length - i >= sizeof("<tt>")-1){
+                            if(memcmp(text+i, "<tt>", sizeof("<tt>")-1) == 0){
+                                msb_write_literal(sb, "<tt>");
+                                i += sizeof("<tt>")-2;
                                 continue;
                             }
                         }
